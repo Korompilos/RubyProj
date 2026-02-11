400d6200efe4a932fe1c46b5b3cd5d1a3e42a689
KenpachiZar7 <giokorompilis@gmail.com>
2026-02-11 21:06:17 +0200

    posts first commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,49 @@
 class PostsController < ApplicationController
-
   before_action :authenticate_user!
+  before_action :set_post, only: [:edit, :update, :destroy]
 
   def index
+    @lessons = ["Algebra I", "Algebra II", "C#", "Operating Systems", "Databases", "Python", "Android Studio"]
+    
     if params[:category].present?
       @posts = Post.where(category: params[:category]).order(created_at: :desc)
     else
       @posts = Post.all.order(created_at: :desc)
     end
-    @post = Post.new 
+    @post = Post.new
   end
 
   def create
     @post = current_user.posts.build(post_params)
     if @post.save
-      redirect_to posts_path, notice: "Το post αναρτήθηκε!"
+      redirect_to posts_path, notice: "Post made!"
     else
-      render :index
+      redirect_to posts_path, alert: "Something went wrong."
     end
   end
 
+  def edit
+    @lessons = ["Algebra I", "Algebra II", "C#", "Operating Systems", "Databases", "Python", "Android Studio"]
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "Post edited succesfully!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice: "Post deleted!"
+  end
+
   private
+
+  def set_post
+    @post = current_user.posts.find(params[:id]) 
+  end
 
   def post_params
     params.require(:post).permit(:content, :category)
