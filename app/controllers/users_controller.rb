a2f9b15d7be33b3750cf55cce0a8a9914af806ad
KenpachiZar7 <giokorompilis@gmail.com>
2026-02-13 18:41:38 +0200

    add & show friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,10 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    
     @users = User.where.not(id: current_user.id)
+  end
+
+  def my_friends
+    @friends = current_user.friends
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: "posts#index"
   
   get 'find_friends', to: 'users#index'
+  get 'my_friends', to: 'users#my_friends'
   
   devise_for :users, controllers: { 
     omniauth_callbacks: 'users/omniauth_callbacks' 
